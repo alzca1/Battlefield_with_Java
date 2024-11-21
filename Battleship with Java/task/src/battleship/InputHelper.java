87f9fa097b17ee9f593f74cf43afa6b6791ec6cf
Álvaro Zájara <alvaro.zajara@eviden.com>
2024-11-21 08:12:19 +0100

    Added InputHelper class

---
+++ Battleship with Java/task/src/battleship/InputHelper.java
@@ -0,0 +1,22 @@
+package battleship;
+
+import java.util.Scanner;
+
+public class InputHelper {
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public int getInt(String message){
+        System.out.println(message);
+        return scanner.nextInt();
+    }
+
+    public String getString(String message){
+        System.out.println(message);
+        return scanner.nextLine();
+    }
+
+    public void closeScanner(){
+        scanner.close();
+    }
+}
